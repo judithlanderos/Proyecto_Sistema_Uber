abc01f8d434bc2c5ff6820c541e1d81c99de0a9c
Judith Landeros <judith.lt@tecjerez.edu.mx>
2026-05-04 00:23:53 -0600

    3.Commit

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
+import Registro from '../views/Registro.vue'
+import Dashboard from '../views/Dashboard.vue'
+import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+    {path: '/',  redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/registro', component: Registro },
+    { path: '/dashboard', component: Dashboard },
+    { path: '/:pathMatch(.*)*', component: NotFound }
   ],
 })
 
