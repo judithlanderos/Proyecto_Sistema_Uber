4be3aebdd6850852fa42fb6fa5316bc8b1e80505
Judith Landeros <judith.lt@tecjerez.edu.mx>
2026-05-09 15:44:39 -0600

    17.Commit models  backend

---
+++ backend/models/viajeModel.js
@@ -0,0 +1,71 @@
+const db = require('../config/database');
+
+const obtenerViajes = (callback) => {
+    const sql = `
+        SELECT 
+            v.id_viaje,
+            CONCAT(u.nombre, ' ', u.primer_ap) AS pasajero,
+            CONCAT(c.nombre, ' ', c.primer_ap) AS conductor,
+            ve.placa AS vehiculo,
+            v.origen,
+            v.destino,
+            v.estado,
+            v.monto_cobrado,
+            v.fecha_salida
+        FROM Viaje v
+        JOIN Usuario u ON v.Usuario_id_usuario = u.id_usuario
+        JOIN Conductor c ON v.Conductor_id_conductor = c.id_conductor
+        JOIN Vehiculo ve ON v.Vehiculo_id_vehiculo = ve.id_vehiculo
+        ORDER BY v.id_viaje DESC
+    `
+    db.query(sql, callback)
+}
+
+const agregarViaje = (datos, callback) => {
+    const sql = 'INSERT INTO Viaje (Usuario_id_usuario, Conductor_id_conductor, Vehiculo_id_vehiculo, origen, destino, fecha_salida, estado, monto_cobrado) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+    db.query(sql, datos, callback)
+}
+
+const editarViaje = (datos, callback) => {
+    const sql = 'UPDATE Viaje SET origen = ?, destino = ?, estado = ?, monto_cobrado = ? WHERE id_viaje = ?'
+    db.query(sql, datos, callback)
+}
+
+const eliminarViaje = (id, callback) => {
+    const sql = 'DELETE FROM Viaje WHERE id_viaje = ?'
+    db.query(sql, [id], callback)
+}
+
+const obtenerUsuarios = (callback) => {
+    db.query('SELECT id_usuario, nombre, primer_ap FROM Usuario', callback)
+}
+
+const obtenerConductores = (callback) => {
+    db.query('SELECT id_conductor, nombre, primer_ap FROM Conductor', callback)
+}
+
+const obtenerVehiculos = (callback) => {
+    db.query('SELECT id_vehiculo, placa, marca, modelo FROM Vehiculo', callback)
+}
+
+const obtenerConteos = (callback) => {
+    const sql = `
+        SELECT
+            (SELECT COUNT(*) FROM Usuario) AS usuarios,
+            (SELECT COUNT(*) FROM Conductor) AS conductores,
+            (SELECT COUNT(*) FROM Viaje) AS viajes,
+            (SELECT COUNT(*) FROM Pago) AS pagos
+    `
+    db.query(sql, callback)
+}
+
+module.exports = {
+    obtenerViajes,
+    agregarViaje,
+    editarViaje,
+    eliminarViaje,
+    obtenerUsuarios,
+    obtenerConductores,
+    obtenerVehiculos,
+    obtenerConteos
+}
---
+++ backend/services/viajeService.js
@@ -0,0 +1,46 @@
+const viajeModel = require('../models/viajeModel')
+
+const listarViajes = (callback) => {
+    viajeModel.obtenerViajes(callback)
+}
+
+const crearViaje = (datos, callback) => {
+    const { id_usuario, id_conductor, id_vehiculo, origen, destino, fecha_solicitud, estado, monto_cobrado } = datos
+    viajeModel.agregarViaje([id_usuario, id_conductor, id_vehiculo, origen, destino, fecha_solicitud, estado, monto_cobrado], callback)
+}
+
+const actualizarViaje = (id, datos, callback) => {
+    const { origen, destino, estado, monto_cobrado } = datos
+    viajeModel.editarViaje([origen, destino, estado, monto_cobrado, id], callback)
+}
+
+const borrarViaje = (id, callback) => {
+    viajeModel.eliminarViaje(id, callback)
+}
+
+const listarUsuarios = (callback) => {
+    viajeModel.obtenerUsuarios(callback)
+}
+
+const listarConductores = (callback) => {
+    viajeModel.obtenerConductores(callback)
+}
+
+const listarVehiculos = (callback) => {
+    viajeModel.obtenerVehiculos(callback)
+}
+
+const listarConteos = (callback) => {
+    viajeModel.obtenerConteos(callback)
+}
+
+module.exports = {
+    listarViajes,
+    crearViaje,
+    actualizarViaje,
+    borrarViaje,
+    listarUsuarios,
+    listarConductores,
+    listarVehiculos,
+    listarConteos
+}
