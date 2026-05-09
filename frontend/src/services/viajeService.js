65e99e86298b4851dd22995cdf1ade054b6f7764
Judith Landeros <judith.lt@tecjerez.edu.mx>
2026-05-09 16:16:58 -0600

    21.Commit services

---
+++ frontend/src/services/viajeService.js
@@ -0,0 +1,11 @@
+import { apiGet, apiPost, apiPut, apiDelete } from '../api/index'
+
+export const getViajes = () => apiGet('/viajes')
+export const postViaje = (datos) => apiPost('/viajes', datos)
+export const putViaje = (id, datos) => apiPut(`/viajes/${id}`, datos)
+export const deleteViaje = (id) => apiDelete(`/viajes/${id}`)
+
+export const getUsuarios = () => apiGet('/usuarios')
+export const getConductores = () => apiGet('/conductores')
+export const getVehiculos = () => apiGet('/vehiculos')
+export const getConteos = () => apiGet('/dashboard/conteos')
