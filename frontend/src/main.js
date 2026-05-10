f0d9bd4eeac7dc4ebc4075d7e674dc9c36e1675a
Judith Landeros <judith.lt@tecjerez.edu.mx>
2026-05-09 18:01:31 -0600

    28.Commit adminlte

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'admin-lte/dist/css/adminlte.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'admin-lte/dist/js/adminlte.min.js'
+
 const app = createApp(App)
 
 app.use(router)
