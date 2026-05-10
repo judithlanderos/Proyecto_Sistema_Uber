1e55a9484ad5f2039c04a637d572f8daf22fbf76
Judith Landeros <judith.lt@tecjerez.edu.mx>
2026-05-09 18:48:37 -0600

    31.Commit main e index

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,7 +7,6 @@ import router from './router'
 import 'admin-lte/dist/css/adminlte.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'admin-lte/dist/js/adminlte.min.js'
-import 'admin-lte/plugins/fontawesome-free/css/all.min.css'
 
 
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import Landing from '../views/LandingPage.vue'
 import Login from '../views/Login.vue'
-
 import Registro from '../views/Registro.vue'
 import Dashboard from '../views/Dashboard.vue'
 import NotFound from '../views/NotFound.vue'
-import LandingPage from '@/views/LandingPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {path: '/',  component: LandingPage },
-    { path: '/login', component: Login },
-    { path: '/registro', component: Registro },
-    { path: '/dashboard', component: Dashboard },
-    { path: '/:pathMatch(.*)*', component: NotFound }
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        { path: '/', component: Landing },
+        { path: '/login', component: Login },
+        { path: '/registro', component: Registro },
+        { path: '/dashboard', component: Dashboard, meta: { requiereAuth: true } },
+        { path: '/:pathMatch(.*)*', component: NotFound }
+    ]
+})
+
+router.beforeEach((to, from, next) => {
+    const token = localStorage.getItem('token')
+    if (to.meta.requiereAuth && !token) {
+        next('/login')
+    } else {
+        next()
+    }
 })
 
 export default router
