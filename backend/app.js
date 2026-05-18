0b689b39b4757db18bd8a100daac41f340fc5101
Judith Landeros <judith.lt@tecjerez.edu.mx>
2026-05-17 22:56:22 -0600

    Modificaciones despliegue app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,14 @@ const path = require('path')
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.use(helmet());
-app.use(cors({ origin: ['http://localhost:5173', 'https://sistemauber-frontend.onrender.com] , credentials: true }));
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        //'https://sistemauber-frontend.onrender.com'
+        'https://proyecto-sistema-uber-1.onrender.com'
+    ],
+    credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
