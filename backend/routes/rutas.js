c611fd13c0e380fb4645ce460982f696087b79fd
Judith Landeros <judith.lt@tecjerez.edu.mx>
2026-05-18 19:18:49 -0600

    modificaicones rutas fotoo

--- backend/routes/rutas.js
+++ backend/routes/rutas.js
@@ -346,15 +346,6 @@ router.delete('/conductores/:id', verificarToken, (req, res) => {
 const multer = require('multer')
 const path = require('path')
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/')
-    },
-    filename: (req, file, cb) => {
-        const ext = path.extname(file.originalname)
-        cb(null, `usuario_${req.params.id}${ext}`)
-    }
-})
 
 const upload = multer({
     storage,
