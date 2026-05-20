a28e5887c3d9b5760e7c16234d7232e364625eaf
Judith Landeros <judith.lt@tecjerez.edu.mx>
2026-05-19 23:19:18 -0600

    Commit width index

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,10 +1,35 @@
 import axios from 'axios'
+import router from '../router'
 
 const BASE_URL =  import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
 
 const getHeaders = () => ({
     Authorization: `Bearer ${localStorage.getItem('token')}`
 })
+
+
+const api = axios.create({ baseURL: BASE_URL })
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('token')
+            localStorage.removeItem('usuario')
+            alert('Tu sesion ha expirado. Por favor vuelve a iniciar sesion.')
+            router.push('/login')
+        }
+        return Promise.reject(error)
+    }
+)
 
 export const apiGet = (url) => axios.get(`${BASE_URL}${url}`, { headers: getHeaders() })
 export const apiPost = (url, data) => axios.post(`${BASE_URL}${url}`, data, { headers: getHeaders() })
