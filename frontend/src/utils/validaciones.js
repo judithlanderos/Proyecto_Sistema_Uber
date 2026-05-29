8acb5b5b697a2571865d5ee47b04b8f30b980deb
Judith Landeros <judith.lt@tecjerez.edu.mx>
2026-05-28 23:22:11 -0600

    Validaciones

--- frontend/src/utils/validaciones.js
+++ frontend/src/utils/validaciones.js
@@ -1,44 +1,57 @@
-const soloLetras = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]*$/
+const soloLetras = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+([a-zA-ZáéíóúÁÉÍÓÚñÑ\s]*[a-zA-ZáéíóúÁÉÍÓÚñÑ]+)?$/
 const soloNumeros = /^[0-9]*$/
-const regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-const regexMonto = /^\d+(\.\d{1,2})?$/
+const regexCorreo = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/
+const regexMonto =  /^(?!0+(\.0{1,2})?$)\d+(\.\d{1,2})?$/
 const regexFecha = /^\d{4}-\d{2}-\d{2}$/
 
 export const validarNombre = (valor) => {
+    const v = (valor ?? '').trim()
     if (!valor) return 'El nombre es obligatorio'
     if (!soloLetras.test(valor)) return 'Solo se permiten letras'
     if (valor.length < 2) return 'Minimo 2 caracteres'
+    if (v.length > 50) return 'El nombre no puede exceder 50 caracteres.'
     return ''
 }
 
 export const validarApellido = (valor, obligatorio = true) => {
+    const v = (valor ?? '').trim()
     if (!valor && obligatorio) return 'El apellido es obligatorio'
     if (valor && !soloLetras.test(valor)) return 'Solo se permiten letras'
+    if (v.length < 2) return 'El apellido debe tener al menos 2 caracteres.'
+    if (v.length > 50) return 'El apellido no puede exceder 50 caracteres.'
     return ''
 }
 
 export const validarCorreo = (valor) => {
+        const v = (valor ?? '').trim()
+
     if (!valor) return 'El correo es obligatorio'
     if (!regexCorreo.test(valor)) return 'Formato invalido, ejemplo: correo@gmail.com'
+    if (v.length > 254) return 'El correo no puede exceder 254 caracteres.'
     return ''
 }
 
 export const validarTelefono = (valor) => {
+    const v = (valor ?? '').trim()
     if (!valor) return 'El telefono es obligatorio'
     if (!soloNumeros.test(valor)) return 'Solo se permiten numeros'
     if (valor.length !== 10) return 'El telefono debe tener 10 digitos'
+    if (/^(\d)\1{9}$/.test(v)) return 'El teléfono no puede tener todos los dígitos iguales.'
     return ''
 }
 
 export const validarPassword = (valor) => {
+    const v = (valor ?? '').trim()
     if (!valor) return 'La contrasena es obligatoria'
     if (valor.length < 6) return 'Minimo 6 caracteres'
     return ''
 }
 
 export const validarMonto = (valor) => {
+    const v = (valor ?? '').toString().trim()
     if (!valor) return ''
     if (!regexMonto.test(valor)) return 'Ingresa un monto valido, ejemplo: 150.00'
+    if (!regexMonto.test(v)) return 'Monto inválido; debe ser mayor a 0, ejemplo: 150.00'
     return ''
 }
 
